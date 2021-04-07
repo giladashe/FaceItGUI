d84d6b11ce6d79082d4eb01954d3891ec3630dce
gilad <giladashe@gmail.com>
2021-04-07 09:48:23 +0300

    Add project files.

---
+++ FaceItGUI/MainWindow.xaml.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Web;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Configuration;
+
+namespace FaceItGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int i = 0;
+        private bool stop = false;
+        private string[] names = { "Gilad", "Israel", "Yossi" };
+
+        private TcpClient Client;
+        volatile Boolean Stop;
+        private NetworkStream Stream;
+
+        private string Port;
+        private string Ip;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Client = null;
+            this.Stop = false;
+            this.Ip = ConfigurationManager.AppSettings["Ip"];
+            this.Port = ConfigurationManager.AppSettings["Port"];
+            Connect(Ip, Convert.ToInt32(Port));
+        }
+        public void Connect(string ip, int port)
+        {
+            try
+            {
+                Stop = false;
+                this.Client = new TcpClient
+                {
+                    ReceiveTimeout = 10000
+                };
+                Client.Connect(ip, port);
+                this.Stream = this.Client.GetStream();
+            }
+            catch
+            {
+
+            }
+        }
+        public void Disconnect()
+        {
+            Stop = true;
+            if (this.Stream != null)
+            {
+                this.Stream.Close();
+                this.Stream = null;
+            }
+            if (this.Client != null)
+            {
+                this.Client.Dispose();
+                this.Client.Close();
+            }
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            string path = Directory.GetCurrentDirectory();
+            string screenshotsDirectory = path + "\\Screenshots";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(screenshotsDirectory);
+            }
+            this.ShowActivated = false;
+            this.Hide();
+            Thread.Sleep(300);
+            var myFrame = SnippingTool.Snip();
+
+            new Thread(delegate ()
+            {
+                // a little depends that row 52 will be executed before other snippinig tool capture
+                string id = names[i];
+                i++;
+                var currentSnip = myFrame.Clone();
+                var actualDirectory = screenshotsDirectory + "\\" + id;
+                if (!Directory.Exists(actualDirectory))
+                {
+                    Directory.CreateDirectory(actualDirectory);
+                }
+                while (!stop)
+                {
+                    // take picture every 0.5 second !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    Thread.Sleep(1000);
+                    try
+                    {
+
+                        long localDate = DateTime.Now.Ticks;
+
+                        string fileName = "\\" + localDate + ".jpg";
+                        Image image = SnippingTool.FromRectangle(currentSnip.Frame);
+                        /* Image image = new Bitmap(currentSnip.Frame.Width, currentSnip.Frame.Height);
+                         using (Graphics gr = Graphics.FromImage(image))
+                         {
+                             gr.DrawImage(this.BackgroundImage, new Rectangle(0, 0, FirstImage.Width, FirstImage.Height),
+                                 rcSelect, GraphicsUnit.Pixel);
+                         }*/
+                        System.Diagnostics.Debug.WriteLine("sending " + actualDirectory + fileName);
+                        WriteToServer(image);
+                        Byte[] data = ReadFromServer();
+                        
+                        MemoryStream ms = new MemoryStream(data);
+                        System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
+                        System.Diagnostics.Debug.WriteLine("got the image: " + actualDirectory + fileName);
+
+                        img.Save(actualDirectory + fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                        System.Diagnostics.Debug.WriteLine(fileName + " sent");
+
+                        /*if (bmp != null)
+                        {
+                            bmp.Save("myImage" + i + ".png");
+                            // Do something with the bitmap
+                            //...
+                            i++;
+                        }*/
+                    }
+                    catch (Exception excep)
+                    {
+                        System.Diagnostics.Debug.WriteLine("problem: " + excep.Message);
+                    }
+                }
+
+
+            }).Start();
+            WindowState = WindowState.Minimized;
+            this.Show();
+            // string folderPath = Server.MapPath("~/ImagesFolder/");  //Create a Folder in your Root directory on your solution.
+            // string fileName = "~/screenshots/IMageName.jpg";
+            /*string imagePath = folderPath + fileName;
+
+            string base64StringData = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAlgAAADmCAYAAAA..........."; // Your base 64 string data
+            string cleandata = base64StringData.Replace("data:image/png;base64,", "");
+            byte[] data = System.Convert.FromBase64String(cleandata);
+            MemoryStream ms = new MemoryStream(data);
+            System.Drawing.Image img = System.Drawing.Image.FromStream(ms);*/
+
+
+
+        }
+
+        public void WriteToServer(Image image, string name)
+        {
+
+            if (image != null && this.Stream != null)
+            {
+
+
+                Byte[] data = ImageToByte(image);
+                System.Diagnostics.Debug.WriteLine("data length write: " + data.Length);
+
+                this.Stream.Write(data, 0, data.Length);
+
+            }
+        }
+
+        public Byte[] ReadFromServer()
+        {
+            bool end = false;
+            if (this.Stream == null)
+            {
+                //this.Error = "First Server!!";
+                return null;
+            }
+            Byte[] data = new Byte[1000000];
+            //StringBuilder response = new StringBuilder();
+            while (!end)
+            {
+                Stream.Read(data, 0, data.Length);
+                /* response.Append(Encoding.ASCII.GetString(data, 0, data.Length));
+                 for (int i = 0; i < 1024; i++)
+                 {
+                     if (data[i] == '\n')
+                     {
+                         end = true;
+                         break;
+                     }
+                 }*/
+                end = true;
+            }
+            // return response.ToString();
+            System.Diagnostics.Debug.WriteLine("data length read: " + data.Length);
+
+            return data;
+
+        }
+
+        public static Byte[] ImageToByte(System.Drawing.Image iImage)
+        {
+            MemoryStream mMemoryStream = new MemoryStream();
+            iImage.Save(mMemoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+            return mMemoryStream.ToArray();
+        }
+    }
+}
---
+++ FaceItGUI/SnippingTool.cs
@@ -0,0 +1,144 @@
+﻿// https://stackoverflow.com/questions/3123776/net-equivalent-of-snipping-tool/3124252#3124252
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FaceItGUI
+{
+    public partial class SnippingTool : Form
+    {
+
+        public static SnippingTool Snip()
+        {
+            var rc = Screen.PrimaryScreen.Bounds;
+            using (Bitmap bmp = new Bitmap(rc.Width, rc.Height, System.Drawing.Imaging.PixelFormat.Format32bppPArgb))
+            {
+                using (Graphics gr = Graphics.FromImage(bmp))
+                    gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                using (var snipper = new SnippingTool(bmp))
+                {
+                    if (snipper.ShowDialog() == DialogResult.OK)
+                    {
+                        return snipper;
+                    }
+                }
+                return null;
+            }
+        }
+
+        public SnippingTool(Image screenShot)
+        {
+            InitializeComponent();
+            this.BackgroundImage = screenShot;
+            this.ShowInTaskbar = false;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            this.DoubleBuffered = true;
+        }
+        public SnippingTool Clone()
+        {
+            var rc = Screen.PrimaryScreen.Bounds;
+            Rectangle recc = new Rectangle(Frame.X, Frame.Y, Frame.Width, Frame.Height);
+            Bitmap bmp = new Bitmap(rc.Width, rc.Height, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+            SnippingTool tool = new SnippingTool(bmp);
+            tool.FirstImage = (Image)this.FirstImage.Clone();
+            tool.Frame = new Rectangle(this.Frame.X, this.Frame.Y, this.Frame.Width, this.Frame.Height);
+
+
+            return tool;
+        }
+        public Image FirstImage { get; set; }
+
+        private Point pntStart;
+        public string TheName { get; set; }
+
+        public Rectangle Frame { get; set; }
+
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            // Start the snip on mouse down
+            if (e.Button != MouseButtons.Left) return;
+            pntStart = e.Location;
+            Frame = new Rectangle(e.Location, new Size(0, 0));
+            this.Invalidate();
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            // Modify the selection on mouse move
+            if (e.Button != MouseButtons.Left) return;
+            int x1 = Math.Min(e.X, pntStart.X);
+            int y1 = Math.Min(e.Y, pntStart.Y);
+            int x2 = Math.Max(e.X, pntStart.X);
+            int y2 = Math.Max(e.Y, pntStart.Y);
+            Frame = new Rectangle(x1, y1, x2 - x1, y2 - y1);
+            this.Invalidate();
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            FirstImage = GetImage();
+        }
+        public Image GetImage()
+        {
+            // Complete the snip on mouse-up
+            // check null!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            if (Frame.Width <= 0 || Frame.Height <= 0) return null;
+            System.Diagnostics.Debug.WriteLine("here 1");
+
+            Image image = new Bitmap(Frame.Width, Frame.Height);
+            System.Diagnostics.Debug.WriteLine("here 2");
+
+            using (Graphics gr = Graphics.FromImage(image))
+            {
+                System.Diagnostics.Debug.WriteLine("here 3");
+
+                gr.DrawImage(this.BackgroundImage, new Rectangle(0, 0, image.Width, image.Height),
+                    Frame, GraphicsUnit.Pixel);
+                System.Diagnostics.Debug.WriteLine("here 4");
+
+            }
+            DialogResult = DialogResult.OK;
+            System.Diagnostics.Debug.WriteLine("here 5");
+
+            return image;
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // Draw the current selection
+            using (Brush br = new SolidBrush(Color.FromArgb(120, Color.White)))
+            {
+                int x1 = Frame.X; int x2 = Frame.X + Frame.Width;
+                int y1 = Frame.Y; int y2 = Frame.Y + Frame.Height;
+                e.Graphics.FillRectangle(br, new Rectangle(0, 0, x1, this.Height));
+                e.Graphics.FillRectangle(br, new Rectangle(x2, 0, this.Width - x2, this.Height));
+                e.Graphics.FillRectangle(br, new Rectangle(x1, 0, x2 - x1, y1));
+                e.Graphics.FillRectangle(br, new Rectangle(x1, y2, x2 - x1, this.Height - y2));
+            }
+            using (Pen pen = new Pen(Color.Red, 3))
+            {
+                e.Graphics.DrawRectangle(pen, Frame);
+            }
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            // Allow canceling the snip with the Escape key
+            if (keyData == Keys.Escape) this.DialogResult = DialogResult.Cancel;
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        public static Image FromRectangle(Rectangle rectangle)
+        {
+            Rectangle rect = new Rectangle(0, 0, 100, 100);
+            Bitmap bmp = new Bitmap(rectangle.Width, rectangle.Height, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+            return bmp;
+        }
+    }
+}
