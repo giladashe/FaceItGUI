52e844744e26110563e4c275dc0d8b59840ef1e6
shaiyis <56585128+shaiyis@users.noreply.github.com>
2021-07-26 15:41:10 +0300

    template modern design fonts

---
+++ FaceItGUI/ModernDeisgn.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FaceItGUI
+{
+  /// <summary>
+  /// Interaction logic for ModernDeisgn.xaml
+  /// </summary>
+  public partial class ModernDeisgn : Window
+  {
+    public ModernDeisgn()
+    {
+      InitializeComponent();
+    }
+  }
+}
